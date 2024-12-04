817da6cecb3dcd750c56c609a6915b5acc8b9379
Cabreo <89184862+Cabreo@users.noreply.github.com>
2024-12-04 17:33:02 +0100

    Update Server and KEY

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["pseha.synology.me"];
+pub const PUBLIC_RS_PUB_KEY: &str = "eYEJ2b96JQlWwzF0FVNFsLvYOgmcaC9kZvGT43jBp6A=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
