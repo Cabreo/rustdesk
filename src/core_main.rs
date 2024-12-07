c58055d9a49d375b339e98818f6e6a2ba4ccf505
Cabreo <89184862+Cabreo@users.noreply.github.com>
2024-12-07 11:58:45 +0100

    Add files via upload

--- src/core_main.rs
+++ src/core_main.rs
@@ -308,16 +308,15 @@ pub fn core_main() -> Option<Vec<String>> {
             return None;
         } else if args[0] == "--password" {
             if args.len() == 2 {
-			   crate::ipc::set_permanent_password(args[1].to_owned())
-               // if crate::platform::is_installed() && is_root() {
-               //     if let Err(err) = crate::ipc::set_permanent_password(args[1].to_owned()) {
-               //         println!("{err}");
-               //     } else {
-               //         println!("Done!");
-               //     }
-               // } else {
-               //     println!("Installation and administrative privileges required!");
-               // }
+                if crate::platform::is_installed() && is_root() {
+                    if let Err(err) = crate::ipc::set_permanent_password(args[1].to_owned()) {
+                        println!("{err}");
+                    } else {
+                        println!("Done!");
+                    }
+                } else {
+                    println!("Installation and administrative privileges required!");
+                }
             }
             return None;
         } else if args[0] == "--set-unlock-pin" {
